9aae8b953b679d95b269c40aa1f2b01dc8b22ed5
Alexander Greene <greene.alexanderj@gmail.com>
2018-09-16 00:21:34 -0400

    added several user choice methods to VirtualPet

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -2,4 +2,51 @@ package virtual_pet;
 
 public class VirtualPet {
 
+	int hunger;
+	int thirst;
+	int waste;
+	int boredom;
+	int health;
+	int happiness;
+	boolean disease;
+
+	public VirtualPet() {
+
+	}
+
+	public void tick() {
+		hunger = +1;
+		thirst = +1;
+		waste = +1;
+		boredom = +1;
+
+	}
+
+	public void feed(int foodType) {
+		if (foodType == 1) {
+			hunger = -2;
+			thirst = -2;
+			waste = +2;
+			tick();
+		} else if (foodType == 2) {
+			hunger = -4;
+			thirst = +1;
+			waste = +3;
+			tick();
+		} else if (foodType == 3) {
+			hunger = -6;
+			thirst = +2;
+			waste = +3;
+			tick();
+		}
+
+	}
+
+	public void play(String activityType) {
+		hunger = +1;
+		thirst = +1;
+		boredom = -1;
+
+	}
+
 }
--- src/main/java/virtual_pet/VirtualPetUser.java
+++ src/main/java/virtual_pet/VirtualPetUser.java
@@ -1,7 +1,61 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class VirtualPetUser {
 	public static void main(String[] args) {
+		System.out.println("A strange man approaches you.");
+		System.out.println("He sidles up to you and speaks to you without looking directly at you.");
+		System.out.println();
+		System.out.println("\"Eh, man. You want this egg?\"");
+
+		Scanner input = new Scanner(System.in);
+		boolean eggResponse = false;
+		do {
+			String yesNo = input.next();
+
+			if (yesNo.toLowerCase().equals("no")) {
+				System.out.println("\"Fine.\"");
+				System.out.println("\"Your loss.\"");
+				System.out.println("\"Bye now\"");
+				System.out.println();
+				System.out.println("He leaves. You are filled with a profound sense of regret.");
+				System.out.println("The curiousity over what could have been eventually kills you.");
+				System.out.println("Sorry about your luck.");
+				System.exit(0);
+			} else if (yesNo.toLowerCase().equals("yes")) {
+				System.out.println("\"You do? Nice!\"");
+				System.out.println("He hands you the egg.");
+				System.out.println("\"Good luck!\"");
+				System.out.println("You blink and he is gone.");
+				System.out.println("Now you have this egg.");
+				System.out.println();
+				System.out.println("Some time passes.");
+				System.out.println();
+				System.out.println("Now the egg is hatching.");
+				System.out.println();
+				eggResponse = false;
+			} else {
+				System.out.println("\"What?\"");
+				System.out.println("\"Do you want this egg or not?\"");
+				eggResponse = true;
+			}
+
+		} while (eggResponse);
+
+		VirtualPet newVirtualPet = new VirtualPet();
+
+		System.out.println("Congratulations. It's a...thing.");
+		System.out.println();
+		System.out.println("What do you want to do?");
+		int activity = input.nextInt();
+		System.out.println("1 - Feed");
+		System.out.println("2 - Play");
+		System.out.println("3 - Give Drink");
+		System.out.println("4 - Go to the Doctor");
+		System.out.println("5 - Clean");
+
+		input.close();
 
 	}
 
